654beff74b339f0a7cfdfb60c76aca9524624499
keviny11 <47467282+keviny11@users.noreply.github.com>
2021-11-10 18:51:45 +0800

    updated project instructions

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Web3Research
+
+*We have the class `Address Monitor`*
+
+1. Acquire an API key from https://etherscan.io/myapikey
+2. Set up an Infura node at https://infura.io/
+3. The idea is to retrieve the latest transactions of an account using the etherscan API, since we want to keep track of what we've been notified, use a `latest_block` variable to limit the block range to search
+4. Obtain a URL to sent POST requests to a slack channel, reference video: https://www.youtube.com/watch?v=lEQ68HhpO4g&ab_channel=TechandBeyondWithMoss
+5. build_slack_payload creates customized slack messages (in progress)
+
+*Problems to be solved*
+1. Figure out what coin each "to" address resolves to
+2. Make slack messages more readable
+
+
--- web3main.py
+++ web3main.py
@@ -1,60 +1,69 @@
-# from web3 import Web3
-
-# w3 = Web3(Web3.HTTPProvider('https://mainnet.infura.io/v3/d7af83af047d4230805fec39b32597af'))
-# balance = w3.eth.get_balance('0xDDa5d738fDcD721e34E83cD5D7d5596c8AAef549')
-
-# print(balance)
-
+from web3 import Web3
+from web3.exceptions import BlockNotFound
 import requests
 import time
 import json
+from datetime import datetime
 
-api_key = 'FHA2FHG2E3GFZTQ26WRP6X46452U9Z8AQ7'
 
 class AddressMonitor:
 
     def __init__(self):
+        self.web3 = Web3(Web3.HTTPProvider('https://mainnet.infura.io/v3/d7af83af047d4230805fec39b32597af'))
+        self.api_key = 'FHA2FHG2E3GFZTQ26WRP6X46452U9Z8AQ7'
+        self.slack_link = 'https://hooks.slack.com/services/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'
         with open('./address.json') as f:
             self.addresses = json.load(f)[0]
-        self.record = self.get_transactions()
+        self.latest_block = self.web3.eth.block_number - 1
+
+    def get_block_range(self):
+        start_block = self.latest_block
+        end_block = self.web3.eth.block_number
+        self.latest_block = end_block
+        return start_block, end_block
 
     def address_transaction_url_formatter(self, address):
+        start_block, end_block = self.get_block_range()
+        if start_block == end_block:
+            return None
         return \
         'https://api.etherscan.io/api' \
         + '?module=account' \
         + '&action=txlist' \
         + f'&address={address}' \
-        + '&startblock=0' \
-        + '&endblock=99999999' \
-        + '&sort=asc' \
-        + f'&apikey={api_key}'
+        + f'&startblock={start_block}' \
+        + f'&endblock={end_block}' \
+        + '&sort=desc' \
+        + f'&apikey={self.api_key}'
+    
+    def build_slack_payload(self, record):
+        pass
 
     def get_transactions(self):
         new_record = {}
-        for alias in self.addresses.keys():
-            address = self.addresses[alias]
+        update = False
+        for address in self.addresses.keys():
             url = self.address_transaction_url_formatter(address)
-            response = requests.get(url)
-            response_obj = json.loads(response.content)
-            new_record[address] = response_obj['result']
-        return new_record
-            
-    def get_new_transactions(self):
-        new_record = self.get_transactions()
-        diff = {}
-        for k in new_record.keys():
-            print(new_record[k][-1]['hash'])
-            for i, entry in enumerate(reversed(new_record[k])):
-                if self.record[k][-1]['hash'] == entry['hash']:
-                    self.record = new_record
-                    break
-                else:
-                    print(entry['hash'], i)
+            if url:
+                response = requests.get(url)
+                response_obj = json.loads(response.content)
+                new_record[address] = [{'hash': entry['hash'], 'from': entry['from'], 'to': entry['to'], 'value': float(entry['value'])/1e18} for entry in response_obj['result']]
+                if response_obj['result']:
+                    update = True
+        if update:
+            for k in new_record.keys():
+                if not new_record[k]:
+                    del new_record[k]
+            payload = self.build_slack_payload(new_record)
+            new_record = '*' + json.dumps(new_record, indent=4) + '*'
+            response = requests.post(self.slack_link, data=json.dumps({'text': new_record}, indent=4))
+            print(response.content)
 
-    def get_new_transactions_loop(self):
+    def get_transactions_loop(self):
         while True:
-            self.get_new_transactions()
-            time.sleep(1)
+            self.get_transactions()
+            time.sleep(10)
 
-address_monitor = AddressMonitor()
-address_monitor.get_new_transactions_loop()
+if __name__ == '__main__':
+    address_monitor = AddressMonitor()
+    address_monitor.get_transactions_loop()
